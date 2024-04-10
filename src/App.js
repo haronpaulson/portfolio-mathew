c2df06f952967b87cea6abca4bacd981a8141839
Haron Paulson <haron.paulson@anitechgroup.com>
2024-04-10 17:52:24 +0530

    Add zoom out anim

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import logo from './logo.svg';
 import Header from './Header';
 import Mainpage from './Mainpage';
 import Skillbox from './Skillbox';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import React, {useEffect} from 'react';
 
 import './App.css';
 
+
 function App() {
+  useEffect(() => {
+    AOS.init({duration: 2000})
+  }, [])
+
   return (
-    <div className="App">
+    <div className="App" data-aos="zoom-in" >
 
     
       <Mainpage />
+      <div className=' flex justify-center bg-black'>
+      <h1 className=' text-white'>Mathew loves</h1>
+      </div>
+     
     </div>
   );
 }
